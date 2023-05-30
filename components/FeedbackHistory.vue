0cf42a764d2216b10c03c1da73205bf1cbc72cc3
Miltonbhowmick <miltonbhowmick6@gmail.com>
2023-05-30 17:18:49 +0600

    Fixed gap issues between each sections headline subheadline

--- components/FeedbackHistory.vue
+++ components/FeedbackHistory.vue
@@ -73,10 +73,7 @@
 <style scoped lang="scss">
 .feedback-box {
     margin: 50px 0px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 8px;
+
     .client-history {
         margin: 100px 0px;
         display: flex;
