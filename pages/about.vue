b161d10c0151250d553da4ace8ea3db35fa45c53
Miltonbhowmick <miltonbhowmick6@gmail.com>
2023-06-19 20:12:57 +0600

    Added about page

---
+++ pages/about.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container">
+        <div class="aboutus below-header-gap">
+            <h1 class="headline">
+                Bifrost is a term that holds significance in Norse mythology and
+                in the realm of technology. Let's explore both aspects
+            </h1>
+            <div class="detail-box">
+                <h2 class="detail">
+                    In Norse Mythology:<br />
+                    In Norse mythology, Bifrost, also known as the Rainbow
+                    Bridge, is a celestial bridge that connects the realm of
+                    Asgard, the home of the gods, with Midgard, the realm of
+                    humans. It is described as a shimmering, multicolored bridge
+                    that spans the gap between these two realms. Bifrost is
+                    guarded by the god Heimdall, who has the ability to see and
+                    hear everything that occurs across the cosmos. According to
+                    the myth, Bifrost will break and collapse during the events
+                    of Ragnarok, the final battle that leads to the destruction
+                    of the world.
+                </h2>
+                <h3 class="detail">
+                    In Technology:<br /> Bifrost is also the name of a technology used
+                    in computer graphics and rendering. It is the codename given
+                    to the GPU (Graphics Processing Unit) architecture developed
+                    by the company ARM Holdings. ARM is a British semiconductor
+                    and software design company that specializes in the
+                    development of processors used in a wide range of devices,
+                    including smartphones, tablets, and embedded systems.
+                </h3>
+                <h4 class="detail">
+                    The Bifrost architecture is designed to provide
+                    high-performance graphics rendering capabilities for mobile
+                    devices. It offers improved efficiency, increased
+                    performance, and advanced features for graphics processing.
+                    The architecture supports features like advanced shading
+                    techniques, virtual reality rendering, and machine learning
+                    acceleration, among others. Bifrost-based GPUs are used in
+                    many popular smartphones and tablets, delivering enhanced
+                    graphics and gaming experiences to users.
+                </h4>
+                <h5 class="detail">
+                    It's worth noting that since my knowledge cutoff is in
+                    September 2021, there might have been further developments
+                    or newer technologies associated with the term "Bifrost"
+                    since then.
+                </h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.aboutus {
+    .headline {
+        margin: 60px 0px;
+        font-weight: 600;
+        font-size: 30px;
+        line-height: 44px;
+    }
+    .detail-box {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 50px;
+        .detail {
+            font-weight: 400;
+            font-size: 20px;
+            line-height: 44px;
+        }
+    }
+}
+</style>
